19150c26c19a38e73c2841c7113474d70ef1ef61
Shailendra Singh <b4shailen@gmail.com>
2023-03-10 15:49:14 +0530

    Update CrashController.java

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
-	* Shailendra is woring on it.
  * Controller used to showcase what happens when an exception is thrown
  *
  * @author Michael Isvy
